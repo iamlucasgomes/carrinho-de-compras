033a7b6f8d1b3187bcefe4b80649e3fe70c9730e
Lucas <lucas.devjs@gmail.com>
2022-08-13 16:08:13 -0300

    UPDATE: teste se salva o preço e o cart no localstorage após remover um item

--- script.js
+++ script.js
@@ -39,6 +39,8 @@ const getSkuFromProductItem = (item) => item.querySelector('span.item__sku').inn
 
 const cartItemClickListener = (event) => {
   event.target.remove();
+  saveCartItems(cartItems.innerHTML);
+  saveTotalPrice(totalPrice.innerHTML);
 };
 
 const createCartItemElement = ({ sku, name, salePrice, image }) => {
